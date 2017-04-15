60d9df0800ae5caf588214a95d5fb03e4b53d3c4
Sol <sol@Sols-MBP.ccs>
2017-04-15 01:39:16 -0400

    Add functional nav menu with SVG filter animations and links

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import {
   Link
 } from 'react-router-dom'
 
+import Home from './components/home.js'
+
 const supportsHistory = 'pushState' in window.history
 
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
+
 
 const About = () => (
   <div>
@@ -56,21 +54,54 @@ const Topics = ({ match }) => (
 
 const BasicExample = () => (
   <Router
-    forceRefresh={!supportsHistory}
+
   >
     <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-      </ul>
+      <div dangerouslySetInnerHTML={{__html:
+          `<nav class="menu">
+            <input type="checkbox" href="#" class="menu-open" name="menu-open" id="menu-open"/>
+            <label class="menu-open-button" for="menu-open">
+              <span class="hamburger hamburger-1"></span>
+              <span class="hamburger hamburger-2"></span>
+              <span class="hamburger hamburger-3"></span>
+            </label>
 
+            <a href="#" class="menu-item"> <i class="fa fa-bar-chart"></i> </a>
+            <a href="#" class="menu-item"> <i class="fa fa-plus"></i> </a>
+            <a href="/topics" class="menu-item"> <i class="fa fa-heart"></i> </a>
+            <a href="#" class="menu-item"> <i class="fa fa-envelope"></i> </a>
+            <a href="/" class="menu-item"> <i class="fa fa-cog"></i> </a>
+
+          </nav>
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+              <defs>
+                <filter id="shadowed-goo">
+
+                    <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
+                    <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+                    <feGaussianBlur in="goo" stdDeviation="3" result="shadow" />
+                    <feColorMatrix in="shadow" mode="matrix" values="0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 -0.2" result="shadow" />
+                    <feOffset in="shadow" dx="1" dy="1" result="shadow" />
+                    <feBlend in2="shadow" in="goo" result="goo" />
+                    <feBlend in2="goo" in="SourceGraphic" result="mix" />
+                </filter>
+                <filter id="goo">
+                    <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
+                    <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+                    <feBlend in2="goo" in="SourceGraphic" result="mix" />
+                </filter>
+              </defs>
+          </svg>`
+          }}>
+
+      </div>
       <hr/>
 
       <Route exact path="/" component={Home}/>
       <Route path="/about" component={About}/>
       <Route path="/topics" component={Topics}/>
-    </div>
+  </div>
+
   </Router>
 )
 export default BasicExample
